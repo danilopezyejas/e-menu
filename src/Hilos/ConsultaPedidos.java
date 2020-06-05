c99797f395d1b7abc5339ee0b59b322c471525fc
lescano <lescanov24@gmail.com>
2020-06-04 22:16:34 -0300

    Hilos

--- src/Hilos/ConsultaPedidos.java
+++ src/Hilos/ConsultaPedidos.java
@@ -9,6 +9,8 @@ import Logica.Pedidos;
 import Persistencia.Conexion;
 import java.awt.Color;
 import static java.lang.Thread.sleep;
+import java.util.AbstractList;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Timer;
@@ -33,20 +35,49 @@ public class ConsultaPedidos extends Thread {
         //quede consultando los pedidos, esto se termina cuando se cierra el hilo 
         //que sería antes de que se cierre la ventana atención
         while (true){
-//            String QUERY = "SELECT pedidos.id, Pedidos.alimentos_cantidad, Pedidos.estado, Pedidos.mesa_id"
-//                    + " FROM Pedidos INNER JOIN Mesa on Pedidos.mesa_id=Mesa.id";
-//            EntityManager em = Conexion.getInstance().getEntity();
-//            List<Pedidos> ret = em.createQuery(QUERY, Pedidos.class).getResultList();
-
-            for(int i=0; i<arregloBotones.length;i=i+2){
-                Pedidos p = new Pedidos();//(Pedidos) ret.get(i);
-                p.setFecha_hora(new Date());
-                JButton botonx = (JButton) arregloBotones[i];
-                botonx.setBackground(Color.green);
-                temporizador(botonx);
-            }
+            String QUERY = "SELECT p.* "
+                    + "FROM pedidos  p, mesa where p.mesa_id=mesa.id";
+            EntityManager em = Conexion.getInstance().getEntity();
+            List<Pedidos> ret = new ArrayList<Pedidos>();
+            em.getTransaction().begin();
             try {
-                sleep(500);
+                ret = em.createNativeQuery(QUERY, Pedidos.class).getResultList();
+                em.getTransaction().commit();
+            
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                em.getTransaction().rollback();
+            }
+            if (ret.size()>0){
+                //System.out.println("hay pedidos");
+//                for (int j=0; j<ret.size(); j++){
+//                    Pedidos p = (Pedidos) ret.get(j);
+//                    
+//                    System.out.println("id: "+p.getId());
+//                }
+
+                for(int i=0; i<ret.size();i++){
+                    Pedidos p = (Pedidos) ret.get(i);
+                    for(int j=0; j<arregloBotones.length;j++){
+                        String nombreBoton = "btnMesa" + p.getMesa().getNumeroMesa();
+                        if (nombreBoton.compareTo(arregloBotones[j].getName()) == 0){
+                            JButton botonx = (JButton) arregloBotones[j];
+                            //botonx.setBackground(Color.green);
+                            System.out.println("Datos "+j);
+                            System.out.println("en el arreglo "+arregloBotones[j].getName());
+                            System.out.println("nombrebtn "+nombreBoton);
+                            ConsultaPedidos hilo_auxiliar = new ConsultaPedidos();
+                            hilo_auxiliar.temporizador(botonx);
+                            //temporizador(botonx);
+                        }
+                    }
+                }
+            }else{
+                System.out.println("No hay pedidos pendientes");
+            }
+            
+            try {
+                sleep(30 * 1000);
             } catch (InterruptedException ex) {
                 Logger.getLogger(ConsultaPedidos.class.getName()).log(Level.SEVERE, null, ex);
             }
@@ -57,21 +88,19 @@ public class ConsultaPedidos extends Thread {
         this.arregloBotones = arreglo;
     }
     
-    void temporizador(JButton b){
+    public void temporizador(JButton b){
         Timer timer;
         TimerTask tarea;
-        int parpadeo = 5000;
+        int parpadeo = 1000;
         
         tarea = new TimerTask(){
             @Override
             public void run() {
                  if(cambiar){
-                    ImageIcon icon = new ImageIcon("../Imagenes/Mesa con pedido 1.png");
-                    b.setIcon(icon);
+                    b.setBackground(Color.black);
                     cambiar =false;
                 }else{
-                    ImageIcon icon = new ImageIcon("../Imagenes/Mesa con pedido 2.png");
-                    b.setIcon(icon);
+                    b.setBackground(Color.red);
                     cambiar =true;
                 }
             }
